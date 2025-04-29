c9205fccc37e8bd40dd7a18a3d726d933ea52908
Felipe Moreira <felipe_moreira97@hotmail.com>
2025-04-29 20:11:33 -0300

    Create tests to App.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,63 @@
 import React from 'react';
 import App from './App';
-import { screen, render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom'
 
-it('should render', () => {
+it('should render Login page', () => {
   render(<App />);
+  expect(screen.getByText("Login")).toBeInTheDocument()
 });
+
+it('should return error message', () => {
+  render(<App />);
+  fireEvent.change(screen.getByLabelText(/Email/i), {
+    target: { value: 'chuck' },
+  })
+  fireEvent.change(screen.getByLabelText(/Senha/i), {
+    target: { value: 'norris' },
+  })
+  fireEvent.click(screen.getByText(/Acessar/i))
+  expect(screen.getByText(/Usuário ou senha incorretos!/i)).toBeInTheDocument()
+})
+
+it('should return success message', () => {
+  render(<App />);
+  fireEvent.change(screen.getByLabelText(/Email/i), {
+    target: { value: 'eduardo.lino@pucpr.br' },
+  })
+  fireEvent.change(screen.getByLabelText(/Senha/i), {
+    target: { value: '123456' },
+  })
+  fireEvent.click(screen.getByText(/Acessar/i))
+  expect(screen.getByText(/Acessado com sucesso!/i)).toBeInTheDocument()
+})
+
+it('should unmount succsess message after reset', () => {
+  render(<App />);
+  fireEvent.change(screen.getByLabelText(/Email/i), {
+    target: { value: 'eduardo.lino@pucpr.br' },
+  })
+  fireEvent.change(screen.getByLabelText(/Senha/i), {
+    target: { value: '123456' },
+  })
+  fireEvent.click(screen.getByText(/Acessar/i))
+  expect(screen.getByText(/Acessado com sucesso!/i)).toBeInTheDocument()
+
+  fireEvent.click(screen.getByText(/Redefinir/i))
+  expect(screen.queryByText(/Acessado com sucesso!/i)).not.toBeInTheDocument()
+})
+
+it('should unmount error message after reset', () => {
+  render(<App />);
+  fireEvent.change(screen.getByLabelText(/Email/i), {
+    target: { value: 'eduardo.lino@pucpr.br' },
+  })
+  fireEvent.change(screen.getByLabelText(/Senha/i), {
+    target: { value: '123123' },
+  })
+  fireEvent.click(screen.getByText(/Acessar/i))
+  expect(screen.getByText(/Usuário ou senha incorretos!/i)).toBeInTheDocument()
+
+  fireEvent.click(screen.getByText(/Redefinir/i))
+  expect(screen.queryByText(/Usuário ou senha incorretos!/i)).not.toBeInTheDocument()
+})
