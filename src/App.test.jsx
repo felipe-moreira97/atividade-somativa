5714eadd120659384cb0c4d9f10f5a69ac743637
Felipe Moreira <felipe_moreira97@hotmail.com>
2025-04-28 22:26:29 -0300

    Commit with an error test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 import App from './App';
+import { screen } from '@testing-library/react';
 
 it('should renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOMClient.createRoot(div).render(<App />);
 });
+
+it('should be in the screen', () => {
+  expect(screen.getByText('qualquer coisa')).toBeInTheDocument();
+})
