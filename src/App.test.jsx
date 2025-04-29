f0a1ad4522e9e06ca35f28c46669a219349fcb1c
Felipe Moreira <felipe_moreira97@hotmail.com>
2025-04-28 21:54:36 -0300

    Create App.test

---
+++ src/App.test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOMClient from 'react-dom/client';
+import App from './App';
+
+it('should renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOMClient.createRoot(div).render(<App />);
+});
