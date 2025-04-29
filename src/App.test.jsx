8639f4e9e72918901ed37bceef99ea8c66e59da3
Felipe Moreira <felipe_moreira97@hotmail.com>
2025-04-28 22:45:20 -0300

    Update Discord Message

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import ReactDOMClient from 'react-dom/client';
 import App from './App';
-import { screen } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 
-it('should renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOMClient.createRoot(div).render(<App />);
+it('should render', () => {
+  render(<App />);
 });
-
-it('should be in the screen', () => {
-  expect(screen.getByText('qualquer coisa')).toBeInTheDocument();
-})
